9879a466909524f0aceb773a922bb5230842e187
Marco Aneloa <marco.aneloa@msp.gob.ec>
2020-11-23 12:30:11 -0500

    Eventos y useState

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import './index.css'
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(<CounterApp value={123} />, divRoot);
+ReactDOM.render(<CounterApp value={10} />, divRoot);
